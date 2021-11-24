e1406780cf6b163db2d2f120ec545d412d21171d
Cường Võ Dương Quốc <cuongvdq18@uef.edu.vn>
2021-11-24 23:03:58 +0700

    SuaLan1 Thêm thư mục Customer và img

---
+++ Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using ClubPortalMS.Areas.Customer.DAO;
+using System.Web.Mvc;
+using ClubPortalMS.Models;
+
+
+
+namespace ClubPortalMS.Areas.Customer.Controllers
+{
+    public class HomeController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Customer/Home
+        public ActionResult Index()
+        {
+            var ListDAO = new ListDAO();
+            ViewBag.ListNews = ListDAO.ListNews(5);
+            ViewBag.ListHD = ListDAO.listHD(5);
+            ViewBag.Slides = new SlideDao().ListAll();
+            ViewBag.ListAlbums = new ListDAO().ListAlbums(9);
+            return View();
+        } 
+    }
+}
---
+++ Areas/Customer/Controllers/TinTucController.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.Entity;
+using ClubPortalMS.Areas.Customer.DAO;
+using System.Web.Mvc;
+using ClubPortalMS.Models;
+using System.Net;
+using System.Linq;
+
+namespace ClubPortalMS.Areas.Customer.Controllers
+{
+    public class TinTucController : Controller
+    {
+        // GET: Customer/TinTuc
+        ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            var ListDAO = new ListDAO();
+            ViewBag.ListAllNews = ListDAO.ListAllNews();
+            return View(db.TinTucs.ToList());
+        }
+        public ActionResult BaiViet(int? id)
+        {
+            var ListDAO = new ListDAO();
+            ViewBag.ListNews = ListDAO.ListNews(3);
+            ViewBag.ListHD = ListDAO.listHD(3);
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TinTuc tinTuc = db.TinTucs.Find(id);
+            if (tinTuc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tinTuc);
+        }
+
+    }
+}
