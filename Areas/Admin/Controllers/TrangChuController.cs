b7c91f50bbdb4cd595e16092b2b28475966d1046
Cường Võ Dương Quốc <cuongvdq18@uef.edu.vn>
2022-01-11 21:01:43 +0700

    final đồ án

--- Areas/Admin/Controllers/TrangChuController.cs
+++ Areas/Admin/Controllers/TrangChuController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CustomAuthorizationFilter.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,7 +9,8 @@ namespace ClubPortalMS.Areas.Admin.Controllers
 {
     public class TrangChuController : Controller
     {
-        // GET: Admin/TrangChu
+        [CustomAuthenticationFilter]
+        [CustomAuthorize("Admin")]
         public ActionResult Index()
         {
             return View();
