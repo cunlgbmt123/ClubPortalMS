300d0efa16c3cd799d0140dedaa285e6bfd0a5f2
Cường Võ Dương Quốc <cuongvdq18@uef.edu.vn>
2022-01-11 08:31:30 +0700

    Sua lần 1

--- Areas/Admin/AdminAreaRegistration.cs
+++ Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,7 @@ namespace ClubPortalMS.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { Controller = "Dashboard", action = "Index", id = UrlParameter.Optional }
+                new { Controller = "TrangChu", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
